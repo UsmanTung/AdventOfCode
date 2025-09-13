14cbd5e3cee3fe5a32b5974723b0261350edf214
UsmanTung <usman-tung@hotmail.com>
2025-09-13 13:28:12 -0700

    Q3 Part two correct

--- 2024/daythree/daythree.py
+++ 2024/daythree/daythree.py
@@ -1,22 +1,32 @@
 def sumOfMultiplies(input):
     i = 0
     sum = 0
+    do = True
     while i < len(input):
-        if i+3< len(input) and input[i] == "m" and input[i+1] == "u" and input[i+2] == "l" and input[i+3]== "(":
-            i+=4
-            num1 = ""
-            while i<len(input) and isDigit(input[i]):
-                num1 = f"{num1}{input[i]}"
-                i+=1
-            if i < len(input) and isDigit(input[i-1]) and input[i] == ",":
-                i+=1
-                num2 = ""
-                while i <len(input) and isDigit(input[i]):
-                    num2 = f"{num2}{input[i]}"
+        if do:
+            if i+6< len(input) and input[i] == "d" and input[i+1] == "o" and input[i+2] == "n" and input[i+3]== "'" and input[i+4] == "t" and input[i+5] == "(" and input[i+6] == ")":
+                do = False
+            if do and i+3< len(input) and input[i] == "m" and input[i+1] == "u" and input[i+2] == "l" and input[i+3]== "(":
+                i+=4
+                num1 = ""
+                while i<len(input) and isDigit(input[i]):
+                    num1 = f"{num1}{input[i]}"
                     i+=1
-                if i < len(input) and input[i] == ")":
-                    sum+= int(num1) * int(num2)
+                if i < len(input) and isDigit(input[i-1]) and input[i] == ",":
+                    i+=1
+                    num2 = ""
+                    while i <len(input) and isDigit(input[i]):
+                        num2 = f"{num2}{input[i]}"
+                        i+=1
+                    if i < len(input) and input[i] == ")":
+                        sum+= int(num1) * int(num2)
+
+        else:
+            if i+3< len(input) and input[i] == "d" and input[i+1] == "o" and input[i+2] == "(" and input[i+3]== ")":
+                do=True
+
         i+=1
+        
     return sum
 
 def isDigit(c):
