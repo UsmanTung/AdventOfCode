428e8fe0dfedeb5584b3d7244e42bc50b9eb5365
UsmanTung <usman-tung@hotmail.com>
2025-09-22 16:09:33 -0700

    Q11 Part two correct

--- 2024/dayeleven/dayeleven.py
+++ 2024/dayeleven/dayeleven.py
@@ -1,33 +1,32 @@
+from collections import Counter
+
 def afterXBlinks(blinks, input):
-    result = input
+    unique = Counter(input)
     for i in range(blinks):
-        j = 0
-        while j < len(result):
-            if result[j] == "0":
-                result[j] = "1"
-                j += 1
-            elif len(result[j]) % 2 == 0:
-                middle = len(result[j]) // 2
-                left = result[j][0:middle]
-                right = result[j][middle:len(result[j])]
+        count = Counter()
+        for val, c in unique.items():
+            if val == "0":
+                count["1"] += c
+            elif len(val) % 2 == 0:
+                middle = len(val) // 2
+                left = val[0:middle]
+                right = val[middle:len(val)]
                 if int(right) == 0:
                     right = "0"
                 else:
                     while right[0] == "0":
                         right = right[1:len(right)]
-                del result[j]
-                result.insert(j, right)
-                result.insert(j, left)
-                j += 2
-
+                count[left] += c
+                count[right] += c
             else:
-                result[j] = str(int(result[j]) * 2024)
-                j += 1
-    return len(result)
+                count[str(int(val) * 2024)] += c
+        unique = count
+    return sum(unique.values())
 
 def main():
     with open('input.txt', 'r') as file:
         input = file.read().split()
     print(f"after 25 Blinks :{afterXBlinks(25, input)}")
+    print(f"after 75 Blinks :{afterXBlinks(75, input)}")
 if __name__ == "__main__":
     main()
