7185f25aaaa727fe996d4fb4929213b829e000eb
UsmanTung <usman-tung@hotmail.com>
2025-12-06 23:32:54 -0800

    2025 Q2 Part two correct

--- 2025/day2/daytwo.py
+++ 2025/day2/daytwo.py
@@ -1,3 +1,5 @@
+from collections import Counter
+
 def sumOfInvalid(data):
     total = 0
     i = 0
@@ -20,13 +22,35 @@ def sumOfInvalid(data):
         i += 2
     return total
 
+def sumOfInvalid2(data):
+    total = 0
+    i = 0
+    while i+1 < len(data):
+        for j in range(int(data[i]), int(data[i+1])+1):
+            strj = str(j)
+            for y in range(1, len(strj)//2 + 1):
+                if len(strj) % y != 0:
+                    continue
+                valid=True
+                window = y
+                while window + y <= len(strj):
+                    if strj[window - y:window] != strj[window:window + y]:
+                        valid=False
+                        break
+                    window += y
+                if valid:
+                    total += j
+                    break
+        i += 2
+    return total
+
 
 def main():
     with open("2025/day2/input.txt") as f:
         data = f.read().strip()
         data = data.replace("-", ",")
         ranges = data.split(",")
-    print(sumOfInvalid(ranges))
+    print(sumOfInvalid2(ranges))
 
 
 if __name__=="__main__":
